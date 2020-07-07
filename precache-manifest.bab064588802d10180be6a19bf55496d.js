b017ea2269489e63c9cba25f4d67a7845b9421bf
Jared McKinney <jaredmckinney@Jareds-MacBook-Pro.local>
2020-07-07 14:08:48 -0400

    Updates

---
+++ precache-manifest.bab064588802d10180be6a19bf55496d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fde7c16119a0503e769c5314c607a67",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "9534b5534f40cb2c98c7",
+    "url": "/My-Portfolio/static/css/main.c56ed709.chunk.css"
+  },
+  {
+    "revision": "329064ac83df4c7852f7",
+    "url": "/My-Portfolio/static/js/2.b9fc0081.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-Portfolio/static/js/2.b9fc0081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9534b5534f40cb2c98c7",
+    "url": "/My-Portfolio/static/js/main.618c10cd.chunk.js"
+  },
+  {
+    "revision": "bd26aa8a63ab866d9120",
+    "url": "/My-Portfolio/static/js/runtime-main.295a29ee.js"
+  },
+  {
+    "revision": "0737fb7f64363008fdbb680964c7933f",
+    "url": "/My-Portfolio/static/media/ga_state.0737fb7f.png"
+  }
+]);
